1aacace37976b6bc6700a1b209be8024ce1eba76
Rao Hassan <raohassan4212@gmail.com>
2021-07-08 11:47:18 +0500

    get data from global state

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,10 @@
 import Links from "../components/Links";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 
 const Home = () => {
     const history = useHistory();
+    const state = useSelector(state => state);
 
     function goto() {
         history.push("/contact")
@@ -13,6 +15,7 @@ const Home = () => {
         <h1>Home Page</h1>
         <Links />
         <button onClick={goto}>Contact Page</button>
+        <h1>{state.name}</h1>
         </>
     );
 }
