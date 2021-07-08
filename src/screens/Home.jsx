773c593e4e738a3784e5ca5c977babf20cf2f5ac
Rao Hassan <raohassan4212@gmail.com>
2021-07-08 11:44:44 +0500

    useHistory Hook for changing route

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,18 @@
 import Links from "../components/Links";
+import { useHistory } from "react-router";
 
 const Home = () => {
+    const history = useHistory();
+
+    function goto() {
+        history.push("/contact")
+    }
+
     return (
         <>
         <h1>Home Page</h1>
         <Links />
+        <button onClick={goto}>Contact Page</button>
         </>
     );
 }
