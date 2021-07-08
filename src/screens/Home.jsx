f5f8af57bfad690d0f8c28a58ca33ba0af62c6d1
Rao Hassan <raohassan4212@gmail.com>
2021-07-08 15:52:44 +0500

    update the value of global with useDispatch

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,15 @@
 import Links from "../components/Links";
 import { useHistory } from "react-router";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Home = () => {
     const history = useHistory();
     const state = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    function update() {
+        dispatch({type: "UPDATE", name: "Rao Kuamil"});
+    }
 
     function goto() {
         history.push("/contact")
@@ -16,6 +21,7 @@ const Home = () => {
         <Links />
         <button onClick={goto}>Contact Page</button>
         <h1>{state.name}</h1>
+        <button onClick={update}>Update</button>
         </>
     );
 }
--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -2,8 +2,17 @@ const INITIAL_STATE = {
     name: "Rao Hassan"
 };
 
-const Reducer = (state = INITIAL_STATE) => {
-    return state
+const Reducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case "UPDATE":
+           return {
+                ...state,
+                name: action.name
+            }
+    
+        default:
+            return state;
+    }
 }
 
 export default Reducer;
